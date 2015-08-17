924d1e50515577214ff3d5fa8c5a2ce034c6b403
Evan Besser <evan.besser@newscred.com>
2015-08-16 23:34:44 -0400

    changed email processor slightly

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -17,8 +17,9 @@ class EmailProcessor
     	user_id = user_object[0].id
     else
     	User.create(email:user_email, name:user_name, short_name:user_short_name)
-    	user_object = User.where(email:user_email)
-    	user_id = user_object[0].id
+    	user_object = User.where(email:user_email).first
+        # binding.pry
+    	user_id = user_object.id
     end
 
 
