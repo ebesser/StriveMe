2f3842f22c87a897bae0b1233a359c314e3f30e4
Evan Besser <evan.besser@newscred.com>
2015-08-17 23:57:15 -0400

    changed styling for post pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user_short_name = params[:short_name]
     @user = User.where(short_name:user_short_name).first
-    @comments = @user.comments
+    @comments = @user.comments.reverse_order
   end
 end
