81f3c289f1a0eda7ff4d4c1c4cd8b96d8a36f9b7
Evan Besser <evan.besser@newscred.com>
2015-08-25 22:19:07 -0400

    added route for mandrill sync

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Workjounral::Application.routes.draw do
  	get '/' => "comments#index"
 
+ 	get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
+
 	mount_griddler
 
 	resources :users, param: :short_name, :short_name => /[\w.]+/
